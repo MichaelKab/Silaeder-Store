df5ce8e9a07713f640399c9097f212b0ac2ab530
MichaelKab <michael.kabanov4@gmail.com>
2020-09-17 22:54:29 +0300

    manage.py

--- website/admin.py
+++ website/admin.py
@@ -2,5 +2,5 @@ from django.contrib import admin
 from .models import *
 # Register your models here.
 admin.site.register(UserData)
-admin.site.register(transaction)
+admin.site.register(Transaction)
 admin.site.register(Product)
