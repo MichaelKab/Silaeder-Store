7d2c9af1bea629d8777122387bed5b11c1329e26
MichaelKab <michael.kabanov4@gmail.com>
2020-10-02 16:01:20 +0300

    Разбитые модели

---
+++ website/listmodels/__init__.py
@@ -0,0 +1,4 @@
+from website.listmodels.customuser import CustomUser
+from website.listmodels.transaction import Transaction
+from website.listmodels.product import Product
+from website.listmodels.schoolattend import SchoolAttend
---
+++ website/listmodels/transaction.py
@@ -0,0 +1,12 @@
+from django.db import models
+from .customuser import CustomUser
+
+
+class Transaction(models.Model):
+    id_user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    description = models.CharField(max_length=200)
+    cash = models.IntegerField(default=0)
+    date = models.DateTimeField()
+
+    def __str__(self):
+        return "ID:{}  Кол-во:{}  Дата:{}".format(self.id_user.user_id, self.cash, self.date)
