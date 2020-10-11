4b1fa826878cda93d662d1d3a9d678329f883766
sp41mer <sukhanov.aleksandr@yahoo.com>
2020-10-11 20:31:30 +0300

    Добавил фактории для создания тестовых данных Переименовал папку с моделями Добавил простой тест на проверку того, что получается забрать данные

--- website/tests.py
+++ website/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
 
 # Create your tests here.
+from rest_framework import status
+from rest_framework.test import APITestCase, APIRequestFactory
+
+from website.factories import ProductFactory
+
+
+class TestCaseForCity(APITestCase):
+
+    def test_get_city_request_factory(self):
+        city = ProductFactory(name_product="Test", description_product="Test", quantity=10)
+        response = self.client.get("/api/products/")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
